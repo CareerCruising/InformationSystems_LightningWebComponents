b532e21927fe0fba02d60eeff623d24964a647bc
RickFleischmann <rickf@xello.world>
2020-06-15 15:24:48 -0400

    Unit Tests - components and controller - already pushed to Prod

--- UnitTestSql/force-app/main/default/lwc/testSelector/testSelector.js
+++ UnitTestSql/force-app/main/default/lwc/testSelector/testSelector.js
@@ -1,6 +1,5 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import  getDistinctSubTests  from '@salesforce/apex/UnitTestController.getDistinctSubTests';
-import  getSubTestResultList  from '@salesforce/apex/UnitTestController.getSubTestResultList';
 
 export default class TestSelector extends LightningElement {
 
@@ -9,21 +8,21 @@ subTestName;
 subTests;
 subTestResults;
 error;
-columns;
-jsondata;
+@track columns;
+@track jsondata;
+
 
 handleTestClick(evt) {
     this.testName = evt.detail;
     //console.log('==>Selector test chosen: ', evt.detail);
-    console.log('==>Selector handle test: ', this.testName);
+    console.log('==>Selector handleTestClick: ', this.testName);
 
 }
 
 handleSubTestClick(evt) {
     this.subTestName = evt.detail;
     //console.log('==>Selector test chosen: ', evt.detail);
-    console.log('==>Selector handle subtest: ', this.subTestName);
-
+    console.log('==>Selector handleSubTestClick: ', this.subTestName);
 }
 
 @wire(getDistinctSubTests, { TestName: '$testName'})
@@ -31,31 +30,14 @@ wiredSubTests({ error, data }) {
     if (data) {
         this.subTests = data;
         this.error = undefined;
-        console.log('==>Selector subTests: ', this.subTests);
+        console.log('==>Selector getDistinctSubTests: ', this.subTests);
 
     } else if (error) {
         this.error = error;
         this.subTests = undefined;
     }
 }
-  
 
-@wire(getSubTestResultList, { SubTest: '$subTestName'})
-wiredSubTestResults({ error, data }) {
-    if (data) {
-        this.subTestResults = data;
-        this.error = undefined;
-        //console.log('==>Selector subTest results: ', this.subTestResults[0].Json__c);
-        this.jsondata = this.subTestResults[0].Json__c;
-        this.columns = this.subTestResults[0].Columns__c;
-        console.log('==>Selector data: ', data);
-        console.log('==>Selector subTestresults jsondata: ', this.jsondata);
-        console.log('==>Selector subTestresults columns: ', this.columns);
-    } else if (error) {
-        this.error = error;
-        this.subTests = undefined;
-    }
-}
   
 
 }
--- UnitTestSql/force-app/main/default/lwc/testSubTestDetailList/testSubTestDetailList.js
+++ UnitTestSql/force-app/main/default/lwc/testSubTestDetailList/testSubTestDetailList.js
@@ -1,19 +1,54 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import  getSubTestResultList  from '@salesforce/apex/UnitTestController.getSubTestResultList';
+import  getSubTestResultColumns from '@salesforce/apex/UnitTestController.getSubTestResultColumns';
 
 export default class TestSubTestDetailList extends LightningElement {
 
-@api 
-get jsondata() {}
-set jsondata(value) {
-    console.log('jsondata: ', value);
+//j2 = [{App :"CC1",NetWorkName :"Inspire Billings",InspireNetworkId :"61"}]
+
+  @track jsondata;
+  @track cols;
+
+  // Private var to track @api subTestName
+  _subTestName = undefined;
+
+  // Use set and get to process the value every time it's
+  // requested while switching between subtests
+  set subTestName(value) {
+      this._subTestName = value;
+  }
+  
+  // getter for productId
+  @api get subTestName(){
+      return this._subTestName;
+  }
+
+    
+ @wire(getSubTestResultList, { SubTest: '$subTestName'})
+    wiredSubTestResults({ error, data }) {
+    if (data) {
+        this.jsondata = data;
+        this.error = undefined;
+        console.log('==>jsondata ', this.jsondata);
+ 
+    } else if (error) {
+        this.error2 = error;
+        this.jsondata = undefined;
+    }
 }
 
-@api 
-get columns() {}
-set columns(value) {
-    console.log('columns: ', value);
-};
-
+@wire(getSubTestResultColumns, { SubTest: '$subTestName'})
+    wiredSubTestResultCols({ error, data }) {
+    if (data) {
+        this.cols = data;
+        this.error = undefined;
+        console.log('==>cols ', this.cols);
+ 
+    } else if (error) {
+        this.error = error;
+        this.cols = undefined;
+    }
+}
 
 
 }
