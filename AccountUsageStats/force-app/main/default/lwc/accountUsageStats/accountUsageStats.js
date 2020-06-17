95e330a57c557833e764817c22608cb775792d3f
Anthony Abbott <anthonya@careercruising.com>
2020-06-17 00:39:11 -0400

    Next iteration

--- AccountUsageStats/force-app/main/default/lwc/accountUsageStats/accountUsageStats.js
+++ AccountUsageStats/force-app/main/default/lwc/accountUsageStats/accountUsageStats.js
@@ -4,37 +4,59 @@ import { LightningElement, api } from 'lwc';
 const QUERY_URL =
     'https://localhost/api/Integrations/GetUsage?AccId=';
 
-export default class MiscRestCall extends LightningElement {
+export default class app extends LightningElement {
 
-    searchKey = '0014100001dLp2LAAS';
-    data;
+    accountId = '0014100001dLp2LAAS';
+    //searchKey = '0014100001dLp2LAAS';
+    NoStatsFound = null;
+    loading = false;
+    data = [];
     error;
     col = [
-        { fieldName: 'Date', label: 'Date', type: 'date',
-        typeAttributes: {
-            month: 'short',
-            year: 'numeric',
-          },
+        {
+            fieldName: 'Date', label: 'Date', type: 'date',
+            typeAttributes: {
+                month: 'short',
+                year: 'numeric',
+            },
         },
         { fieldName: 'AppDataSource', label: 'Source', type: 'text' },
-        { fieldName: 'SchoolLoginsTotal', label: 'App Logins',  type: 'number' },
-        { fieldName: 'StudentLoginsTotal', label: 'Student Logins', type: 'number' },                   
+        { fieldName: 'SchoolLoginsTotal', label: 'App Logins', type: 'number' },
+        { fieldName: 'StudentLoginsTotal', label: 'Student Logins', type: 'number' },
         { fieldName: 'StudentLoginsUnique', label: 'Unique Logins', type: 'number' },
         { fieldName: 'StudentCount', label: 'Students', type: 'number' },
-        { fieldName: 'UniqueStudentLoginRatio', label: 'Login Ratio', type: 'percent',
-        typeAttributes: {
-            maximumFractionDigits: '1',
-          }, },
-    ];  
+        {
+            fieldName: 'LoginRatio', label: 'Login Ratio', type: 'percent',
+            typeAttributes: {
+                maximumFractionDigits: '1',
+            },
+        },
+    ];
 
+    
+    MonthOptions = [
+            { label: 'Last 24 months', value: '24' },
+            { label: 'Last 12 months', value: '12' },
+            { label: 'Last 6 months', value: '6' },
+            { label: 'Last 3 months', value: '3' },
+        ];
+    MonthOptionSelected = '12';
+
+
+    handleMonthOptionChange(event) {
+        
+        console.log('event.detail',event.detail);
+        this.MonthOptionSelected = event.detail.value;
+        this.fetchUsageStats();
+    }
     handleSearchKeyChange(event) {
-        this.searchKey = event.target.value;
+        this.accountId = event.target.value;
     }
 
-    handleSearchClick() {
-        // The Fetch API is currently not polyfilled for usage in IE11.
-        // Use XMLHttpRequest instead in that case.
-        fetch(QUERY_URL + this.searchKey)
+    fetchUsageStats() {
+        // Use standard Fetch API 
+        this.loading = true;
+        fetch(QUERY_URL + this.accountId + '&Mth=' + this.MonthOptionSelected)
             .then((response) => {
                 // fetch isn't throwing an error if the request fails.
                 // Therefore we have to check the ok property.
@@ -45,10 +67,26 @@ export default class MiscRestCall extends LightningElement {
             })
             .then((jsonResponse) => {
                 this.data = jsonResponse;
+                if (typeof this.data.SiteDataAccount === 'undefined') {
+                    this.error = JSON.stringify(this.data, undefined, 2);
+                    this.data = undefined;
+                } else {
+                    this.NoStatsFound = this.data.SiteDataAccount.length == 0;
+                    this.data.SiteDataAccount.forEach(function(entry) {
+                        entry.LoginRatio = (entry.UniqueStudentLoginRatio/100).toFixed(4);
+                    });
+                }
+                this.loading = false;
             })
             .catch((error) => {
                 this.error = error;
                 this.data = undefined;
             });
+            this.loading = false;
+    }
+    //initialize
+
+    connectedCallback() {
+        this.fetchUsageStats();
     }
 }
