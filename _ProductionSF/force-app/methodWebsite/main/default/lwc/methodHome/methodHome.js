7bd77e7b44910daf104201e439b665276aed76ab
Anthony Abbott <anthonya@careercruising.com>
2022-08-01 09:57:21 -0400

    Latest Updates

--- _ProductionSF/force-app/methodWebsite/main/default/lwc/methodHome/methodHome.js
+++ _ProductionSF/force-app/methodWebsite/main/default/lwc/methodHome/methodHome.js
@@ -10,17 +10,35 @@ export default class MethodHome extends NavigationMixin(LightningElement) {
         SortField: 'Account__r_Name',
         SortAsc: true,     
         ShowInfo: false,   
-        SourceServerOption: [
-            { label: 'AzureUS', value: 'AzureUS' },
-            { label: 'AzureCA', value: 'AzureCA' },
-            { label: 'AzureUK', value: 'AzureUK' },
-            { label: 'CC', value: 'CC' }
+        MonthOptions: [
+            { label: 'Jan', value: '0' },
+            { label: 'Feb', value: '1' },
+            { label: 'Mar', value: '2' },
+            { label: 'Apr', value: '3' },
+            { label: 'May', value: '4' },
+            { label: 'Jun', value: '5' },
+            { label: 'Jul', value: '6' },
+            { label: 'Aug', value: '7' },
+            { label: 'Sep', value: '8' },
+            { label: 'Oct', value: '19' },
+            { label: 'Nov', value: '10' },
+            { label: 'Dec', value: '11' }
         ],
-        FrequencyOption: [
-            { label: 'Nightly', value: 'D' },
-            { label: 'Weekly', value: 'W' },
-            { label: 'Monthly', value: 'M' }
-        ]        
+        DatetePicker: {
+            DateRangeText: '',
+            StartDate: new Date(new Date().getFullYear(),new Date().getMonth(),1),
+            EndDate: new Date(new Date().getFullYear(),new Date().getMonth(),1),
+            MonthStart: 0,
+            MonthEnd: 0,
+            YearStart: 0,
+            YearEnd: 0
+        },
+        OppSimpleStatusOptions: [
+            { label: 'Open', value: 'Open' },
+            { label: 'Closed Won', value: 'Closed Won' },
+            { label: 'Closed Lost', value: 'Closed Lost' }
+        ],
+        OppSimpleStatus: 'Open'
     };
 
     fetchData() {
@@ -40,20 +58,32 @@ export default class MethodHome extends NavigationMixin(LightningElement) {
                         if(entry.SubscriptionEndDate != null) entry.SubscriptionEndDateFormatted = new Date(entry.SubscriptionEndDate).toLocaleDateString('en-US',{year: "numeric", month: "2-digit", day: "2-digit"});
                         
                         //Determine color for bar buttons:
-                        entry.MthTypeColor = (entry.SubscriptionEndDate > new Date()) ? 'btn-on' : '';
-                        entry.MthStudentColor = (entry.MethodStudentActive == 'Y') ? 'btn-on' : '';
-                        entry.MthEducatorColor = (entry.MethodEducatorActive == 'Y') ? 'btn-on' : '';
+                        entry.MthTypeColor = (entry.SubscriptionEndDate > new Date()) ? 'Status-On' : '';
+                        entry.MthStudentColor = (entry.MethodStudentActive == 'Y') ? 'Status-On' : '';
+                        entry.MthEducatorColor = (entry.MethodEducatorActive == 'Y') ? 'Status-On' : '';
 
-                        //Compare for match BEFORE converting to date
-                        entry.DateMatch = true;
-                        if(entry.SubscriptionEndDateFormatted != entry.EndDateFormatted) entry.DateMatch = false;
+                        //Compare to Date / Quantity to Method
+                        entry.QtyMatchClass = (entry.OppSeatQtyParsed == entry.NumberOfAccounts) ? '' : 'slds-text-color_error'; 
+                        entry.NumberOfAccounts = (entry.NumberOfAccounts == null) ? '--' : new Intl.NumberFormat().format((entry.NumberOfAccounts).toFixed(0));
 
-                        entry.QtyMatch = (entry.SeatQtyParsed == entry.NumberOfAccounts) ? true : false; 
+                        entry.DateMatchClass = (entry.SubscriptionEndDateFormatted == entry.EndDateFormatted) ? '' : 'slds-text-color_error';
+                        entry.SubscriptionEndDateFormatted = (entry.SubscriptionEndDateFormatted == null) ? '--': entry.SubscriptionEndDateFormatted;
+
+                        // entry.DateMatch = true;
+                        // if(entry.SubscriptionEndDateFormatted != entry.EndDateFormatted) {
+                        //     entry.DateMatch = false;
+                        // }
                         
+                        // entry.DateMatch = (entry.SubscriptionEndDateFormatted == entry.EndDateFormatted) ? true : false; 
+
+                        // entry.OppSeatQtyParsed = (entry.OppSeatQtyParsed == null) ? '--' : entry.OppSeatQtyParsed;
+
                         //Links
                         entry.AccLink = '/Methodize/s/account?id=' + entry.Account__c
 
-                        entry.TotalPriceCalc = entry.UnitPrice + ' x ' + entry.Quantity ;
+                        
+                        
+                        entry.TotalPriceCalc = entry.UnitPrice + ' x ' + entry.Quantity;
                         entry.RowClass = (entry.OppStageSimple == 'Closed Lost') ? 'rowFadedClass' : '';
                         entry.IsClosedLost = (entry.OppStageSimple == 'Closed Lost') ? true : false;
                     });                    
@@ -68,9 +98,24 @@ export default class MethodHome extends NavigationMixin(LightningElement) {
     }
 
     connectedCallback() {  
+        this.InitializeDatePicker();
         this.fetchData();
     }
 
+    InitializeDatePicker() {  
+        this.PageVar.DatetePicker.StartDate.setMonth(this.PageVar.DatetePicker.StartDate.getMonth() - 2);
+        this.PageVar.DatetePicker.EndDate.setMonth(this.PageVar.DatetePicker.EndDate.getMonth() + 3);
+        this.PageVar.DatetePicker.EndDate.setDate(this.PageVar.DatetePicker.EndDate.getDate() - 1);
+        this.PageVar.DatetePicker.MonthStart = this.PageVar.DatetePicker.StartDate.getMonth();
+        this.PageVar.DatetePicker.MonthEnd = this.PageVar.DatetePicker.EndDate.getMonth();
+        this.PageVar.DatetePicker.YearStart = this.PageVar.DatetePicker.StartDate.getFullYear();
+        this.PageVar.DatetePicker.YearEnd = this.PageVar.DatetePicker.EndDate.getFullYear();
+        this.PageVar.DatetePicker.DateRangeText = this.PageVar.DatetePicker.StartDate.toLocaleDateString('en-us', { year:"numeric", month:"short"}) 
+            + ' - ' + this.PageVar.DatetePicker.EndDate.toLocaleDateString('en-us', { year:"numeric", month:"short"});
+        
+        console.log(this.PageVar.DatetePicker);
+    }
+    
     navigateToSFRecordId(event) {  
         let NavRecordId = event.target.dataset.id;
         console.log('dd');
@@ -127,4 +172,7 @@ export default class MethodHome extends NavigationMixin(LightningElement) {
         //onsole.log(event.target.dataset.popovername);
         this.popover[event.target.dataset.popovername] = false;
     }
+    handleFilterByType(event) {
+        this.PageVar.OppSimpleStatus = event.detail.value;
+    }       
 }
